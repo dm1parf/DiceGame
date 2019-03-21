aec4784dcd7444d7c0c7ad202b74b680df2375fc
Dmitry Parfenov <45736335+dm1parf@users.noreply.github.com>
2019-03-21 22:50:38 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -7,10 +7,34 @@ const rollButton = document.getElementById("roll-button");
 const playerDices = document.getElementById("player-result");
 const computerDices = document.getElementById("computer-result");
 const resultMessage = document.getElementById("result-message");
-const playerDice_img = document.getElementById("player-dice-img");
-const computerDice_img = document.getElementById("computer-dice-img");
+const firstPlayerDice_img = document.getElementById("player-dice-img1");
+const secondPlayerDice_img = document.getElementById("player-dice-img2");
+const firstComputerDice_img = document.getElementById("computer-dice-img1");
+const secondComputerDice_img = document.getElementById("computer-dice-img2");
 
-
+function displayDice(diceValue, element){
+	switch(diceValue){
+		case 1:
+			element.style.backgroundPosition = "top left";
+			break;
+		case 2:
+			element.style.backgroundPosition = "top center";
+			break;
+		case 3:
+			element.style.backgroundPosition = "top right";
+			break;
+		case 4:
+			element.style.backgroundPosition = "bottom left";
+			break;
+		case 5:
+			element.style.backgroundPosition = "bottom center";
+			break;
+		case 6:
+			element.style.backgroundPosition = "bottom right";
+			break;
+		
+	}
+}
 
 function getRandomDice(){
 	let dice = Math.floor(Math.random() * 6 + 1);
@@ -18,59 +42,30 @@ function getRandomDice(){
 }
 
 function game(){
-	let playerDice = getRandomDice();
-	let computerDice = getRandomDice();
-
-	//playerDices.innerHTML = playerDice;
-	//computerDices.innerHTML = computerDice;
-	switch(playerDice){
-		case 1:
-			playerDice_img.style.backgroundPosition = "top left";
-			break;
-		case 2:
-			playerDice_img.style.backgroundPosition = "top center";
-			break;
-		case 3:
-			playerDice_img.style.backgroundPosition = "top right";
-			break;
-		case 4:
-			playerDice_img.style.backgroundPosition = "bottom left";
-			break;
-		case 5:
-			playerDice_img.style.backgroundPosition = "bottom left";
-			break;
-		case 6:
-			playerDice_img.style.backgroundPosition = "bottom right";
-			break;
-		
-	}
-	switch(computerDice){
-		case 1:
-			computerDice_img.style.backgroundPosition = "top left";
-			break;
-		case 2:
-			computerDice_img.style.backgroundPosition = "top center";
-			break;
-		case 3:
-			computerDice_img.style.backgroundPosition = "top right";
-			break;
-		case 4:
-			computerDice_img.style.backgroundPosition = "bottom left";
-			break;
-		case 5:
-			computerDice_img.style.backgroundPosition = "bottom left";
-			break;
-		case 6:
-			computerDice_img.style.backgroundPosition = "bottom right";
-			break;
-		
-	}
+	let firstPlayerDice = getRandomDice();
+	let secondPlayerDice = getRandomDice();
+	let firstComputerDice = getRandomDice();
+	let secondComputerDice = getRandomDice();
+	let computerDices = firstComputerDice + secondComputerDice;
+	let playerDices = firstPlayerDice + secondPlayerDice;
 	
-	if (playerDice > computerDice){
+	displayDice(firstPlayerDice, firstPlayerDice_img);
+	displayDice(secondPlayerDice, secondPlayerDice_img);
+	displayDice(firstComputerDice, firstComputerDice_img);
+	displayDice(secondComputerDice, secondComputerDice_img);
+
+	console.log("First player dice: " + firstPlayerDice);
+	console.log("Second player dice: " + secondPlayerDice);
+	console.log("First computer dice: " + firstComputerDice);
+	console.log("Second computer dice: " + secondComputerDice);
+	console.log("Player score: " + playerDices);
+	console.log("Computer score: " + computerDices);
+	
+	if (playerDices > computerDices){
 		playerScore++;
 		playerScore_span.innerHTML = playerScore;
 		resultMessage.innerHTML = "You win!!!";
-	} else if (playerDice < computerDice){
+	} else if (playerDices < computerDices){
 		computerScore++;
 		computerScore_span.innerHTML = computerScore;
 		resultMessage.innerHTML = "You lost((( T_T";
